761daa5750b6df6195cdea4806cc0d5afc33a308
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2019-07-24 17:56:55 +0900

    added support for LTChar in LTFigure

--- layout_scanner.py
+++ layout_scanner.py
@@ -2,17 +2,25 @@
 
 import sys
 import os
+from tqdm import tqdm
 from binascii import b2a_hex
-
-
-# pdf-miner requirements
+import pandas as pd
+import pickle
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument, PDFNoOutlines
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTFigure, LTImage, LTChar
-
+from logging import getLogger, StreamHandler, Formatter, DEBUG, INFO, WARN
+formatter = Formatter('%(asctime)s %(name)s[%(levelname)s] %(message)s', "%Y-%m-%d %H:%M:%S")
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+handler = StreamHandler()
+handler.setLevel(logger.getEffectiveLevel())
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.propagate = False
 
 def with_pdf(pdf_doc, fn, pdf_pwd, *args):
     """Open the pdf document, and apply the function, returning the results"""
@@ -118,39 +126,58 @@ def to_bytestring(s, enc="utf-8"):
             return s.encode(enc)
 
 
-def update_page_text_hash(h, lt_obj, pct=0.2):
-    """Use the bbox x0,x1 values within pct% to produce lists of associated text within the hash"""
+def update_page_text(df, lt_obj, pct=0.2, logger=logger):
+    """
+    Use the bbox x0,x1 values within pct% to produce lists of associated text within the hash
+
+    df:
+        cols = [x0, y0, x1, y1, class, objs, str]
+    """
+    if df is None: df = pd.DataFrame(columns=['x0', 'y0', 'x1', 'y1', 'class', 'objs', 'str'])
+
+    if isinstance(lt_obj, (LTTextLine, LTTextBox)): store_new_line(df, lt_obj, pct, logger)
+    else:
+        raise NotImplementedError(lt_obj)
+    return df
+
+def store_new_line(df, lt_obj, pct, logger=logger):
+    '''
+    store a new line to df
+    '''
     x0, y0, x1, y1 = lt_obj.bbox
+    candidates = df[
+        (df['class'] == lt_obj.__class__)
+        & (df['x0'] >= x0 * (1 - pct))
+        & (df['x0'] <= x0 * (1 + pct))
+        & (df['x1'] >= x1 * (1 - pct))
+        & (df['x1'] <= x1 * (1 + pct))
+        & (df['y1'] <= y0)
+    ]
 
-    key_found = False
-    for k, v in list(h.items()):
-        hash_x0, hash_x1 = k
-        if x0 >= (hash_x0 * (1.0 - pct)) and (hash_x0 * (1.0 + pct)) >= x0:
-            if x1 >= (hash_x1 * (1.0 - pct)) and (hash_x1 * (1.0 + pct)) >= x1:
-                # the text inside this LT* object was positioned at the same
-                # width as a prior series of text, so it belongs together
-                key_found = True
-                v.append(to_bytestring(lt_obj.get_text()))
-                h[k] = v
-    if not key_found:
-        # the text, based on width, is a new series,
-        # so it gets its own series (entry in the hash)
-        h[(x0, x1)] = [to_bytestring(lt_obj.get_text())]
+    if candidates.shape[0] > 0:
+        if candidates.shape[0] > 1:
+            logger.warn('candidates has shape {}'.format(candidates.shape))
+        target = candidates.iloc[0]
+        df.at[target.name, 'y1'] = y1
+        df.at[target.name, 'objs'].append(lt_obj)
+        df.at[target.name, 'str'].append(to_bytestring(lt_obj.get_text()))
+    else:
+        df.loc[0 if pd.isnull(df.index.max()) else df.index.max() + 1] = [
+            *lt_obj.bbox, lt_obj.__class__, [lt_obj], [to_bytestring(lt_obj.get_text())]
+        ]
+    return df
 
-    return h
-
-
-def parse_lt_objs(lt_objs, page_number, images_folder, text_content=None):
+def parse_lt_objs(lt_objs, page_number, images_folder, text_content=None, logger=logger):
     """Iterate through the list of LT* objects and capture the text or image data contained in each"""
     if text_content is None:
         text_content = []
 
+    page_text = None
     # k=(x0, x1) of the bbox, v=list of text strings within that bbox width (physical column)
-    page_text = {}
-    for lt_obj in lt_objs:
-        if isinstance(lt_obj, (LTTextBox, LTTextLine)):
+    for lt_obj in tqdm(lt_objs, desc='parse objs'):
+        if isinstance(lt_obj, (LTTextBox, LTTextLine, LTChar)):
             # text, so arrange is logically based on its column width
-            page_text = update_page_text_hash(page_text, lt_obj)
+            page_text = update_page_text(page_text, lt_obj)
         elif isinstance(lt_obj, LTImage):
             # an image, so save it to the designated folder, and note its place in the text
             saved_file = save_image(lt_obj, page_number, images_folder)
@@ -160,22 +187,18 @@ def parse_lt_objs(lt_objs, page_number, images_folder, text_content=None):
                     '<img src="' + os.path.join(images_folder, saved_file) + '" />'
                 )
             else:
-                print(
-                    "error saving image on page",
-                    page_number,
-                    lt_obj.__repr__,
-                    file=sys.stderr,
-                )
+                logger.error("failed to save image on page{} {}".format(page_number, lt_obj))
         elif isinstance(lt_obj, LTFigure):
             # LTFigure objects are containers for other LT* objects, so recurse through the children
             text_content.append(
                 parse_lt_objs(lt_obj, page_number, images_folder, text_content)
             )
 
-    for k, v in sorted([(key, value) for (key, value) in list(page_text.items())]):
-        # sort the page_text hash by the keys (x0,x1 values of the bbox),
-        # which produces a top-down, left-to-right sequence of related columns
-        text_content.append("".join(v))
+    if page_text is None:
+        return ''
+
+    page_text = page_text.sort_values('y0')
+    page_text = page_text['str'].apply(lambda x: text_content.append(''.join(x)))
 
     return "\n".join(text_content)
 
@@ -185,12 +208,13 @@ def _parse_pages(doc, images_folder):
     """With an open PDFDocument object, get the pages and parse each one
     [this is a higher-order function to be passed to with_pdf()]"""
     rsrcmgr = PDFResourceManager()
-    laparams = LAParams()
+    laparams = LAParams(detect_vertical=True, all_texts=True)
+    # all_texts will enable layout analysis in LTFigure objs
     device = PDFPageAggregator(rsrcmgr, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
 
     text_content = []
-    for i, page in enumerate(PDFPage.create_pages(doc)):
+    for i, page in tqdm(enumerate(PDFPage.create_pages(doc)), desc='pages'):
         interpreter.process_page(page)
         # receive the LTPage object for this page
         layout = device.get_result()
